e9cbdbbb69eda7f5c02af749f05d16d41a77465e
nidhimamman3 <nidhimamman3@gmail.com>
2024-10-15 19:42:04 +0530

    minor commit

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
-import { ContextProvider } from './Context/ContextProvider.jsx'
+import { ContextProvider } from './Tasks/Context/ContextProvider.jsx'
 
 
 createRoot(document.getElementById('root')).render(
